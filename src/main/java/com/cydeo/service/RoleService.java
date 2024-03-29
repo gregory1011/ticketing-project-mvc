e93136035de7e406b07221d0148f62828ecbcaa1
grigorerosca <gregory081093@gmail.com>
2024-03-28 22:14:26 -0400

    service interfaces

---
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -0,0 +1,13 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.RoleDTO;
+
+public interface RoleService extends CrudService<RoleDTO, Long>{
+
+//    // save role, findById role, delete role, findAll roles
+//    RoleDTO save(RoleDTO role);
+//    RoleDTO findById(Long id);
+//    List<RoleDTO> findAll();
+//    void delete(RoleDTO role);
+//    void deleteById(Long id);
+}
---
+++ src/main/java/com/cydeo/service/UserService.java
@@ -0,0 +1,14 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.UserDTO;
+
+public interface UserService extends CrudService <UserDTO, String> {
+
+//    // save user, findById user, delete user, findAll user
+//    UserDTO save(UserDTO user);
+//    UserDTO findById(String username);
+//    List<UserDTO> findAll();
+//    void delete(UserDTO user);
+//    void deleteById(String username);
+
+}
