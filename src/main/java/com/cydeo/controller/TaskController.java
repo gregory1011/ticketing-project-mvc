bd39fe6d68274486863dafdfb858f866c3d10884
grigorerosca <gregory081093@gmail.com>
2024-04-10 22:14:51 -0400

    employee archive done

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
@@ -76,6 +78,50 @@ public class TaskController {
         return "redirect:/task/create";
     }
 
+
+    @GetMapping("/employee/pendingTasks")
+    public String getPendingTasks(Model model){
+
+        model.addAttribute("tasks", taskService.findTaskByStatusIsNot(Status.COMPLETED));
+
+        return "/task/pending-tasks";
+    }
+
+    @GetMapping("/employee/edit/{id}")
+    public String employeeEditTask(@PathVariable("id") Long id, Model model){
+
+        model.addAttribute("task", taskService.findById(id));
+        model.addAttribute("employees", userService.findEmployee());
+        model.addAttribute("projects", projectService.findAllNonCompletedProjects());
+        model.addAttribute("tasks", taskService.findTaskByStatusIsNot(Status.COMPLETED));
+        model.addAttribute("statuses", Status.values());
+
+        return "/task/status-update";
+    }
+
+//    @PostMapping("/employee/update/{taskId}")
+//    public String employeeUpdateTask(TaskDTO taskDTO){
+//
+//        taskService.updateStatus(taskDTO);
+//        return "redirect:/task/employee/pending-task";
+//    }
+
+    @PostMapping("/employee/update/{id}")
+    public String empUpdateTask(@PathVariable("id") Long id, TaskDTO task){
+
+        task.setTaskId(id);
+        taskService.updateStatus(task);
+
+        return "redirect:/task/employee/pending-tasks";
+    }
+
+    @GetMapping("/employee/archive")
+    public String getArchive(Model model){
+
+        model.addAttribute("task", taskService.findTaskByStatus(Status.COMPLETED));
+
+        return "/task/archive";
+    }
 
 
 
