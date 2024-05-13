c3bd1c91a71bb386a895c1ab1cfbee4dc95b7d8f
grigorerosca <gregory081093@gmail.com>
2024-05-13 11:40:22 -0400

    Role and User service

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -2,12 +2,11 @@ package com.cydeo.service;
 
 import com.cydeo.dto.RoleDTO;
 
-public interface RoleService extends CrudService<RoleDTO, Long>{
+import java.util.List;
 
-//    // save role, findById role, delete role, findAll roles
-//    RoleDTO save(RoleDTO role);
-//    RoleDTO findById(Long id);
-//    List<RoleDTO> findAll();
-//    void delete(RoleDTO role);
-//    void deleteById(Long id);
+public interface RoleService{
+
+    List<RoleDTO> listAllRoles();
+    RoleDTO findById(Long id);
+
 }
--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -1,19 +1,17 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
 
 import java.util.List;
 
-public interface UserService extends CrudService <UserDTO, String> {
+public interface UserService {
 
-//    // save user, findById user, delete user, findAll user
-//    UserDTO save(UserDTO user);
-//    UserDTO findById(String username);
-//    List<UserDTO> findAll();
-//    void delete(UserDTO user);
-//    void deleteById(String username);
-
-    List<UserDTO> findManager();
-    List<UserDTO> findEmployee();
+    List<UserDTO> listAllUsers();
+    UserDTO findByUsername(String username);
+     void save(UserDTO userDTO);
+     UserDTO update(UserDTO userDTO);
+     void deleteByUsername(String username);
+     void delete(String username);
 
 }
