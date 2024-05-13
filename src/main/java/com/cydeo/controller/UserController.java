a2596c61554e12ac284f8b00538d2ef6a6d476b8
grigorerosca <gregory081093@gmail.com>
2024-05-13 11:40:41 -0400

    UserController

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -3,38 +3,46 @@ package com.cydeo.controller;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @Controller
+@AllArgsConstructor
 @RequestMapping("/user")
 public class UserController {
 
 
     // 2 dependency injection, using interface inject for loosing couple
-    RoleService roleService;
-    UserService userService;
+    private final RoleService roleService;
+    private final UserService userService;
 
-    public UserController(RoleService roleService, UserService userService) {
-        this.roleService = roleService;
-        this.userService = userService;
-    }
 
     @GetMapping("/create")
     public String createUser(Model model){
 
 
         model.addAttribute("userDTO", new UserDTO());  // collect the data from UI into new UserDTO
-        model.addAttribute("roles", roleService.findAll()); // bring me all roles from DB
-        model.addAttribute("users", userService.findAll());  // bring all the users from DB
+        model.addAttribute("roles", roleService.listAllRoles()); // bring me all roles from DB
+        model.addAttribute("users", userService.listAllUsers());  // bring all the users from DB
 
         return "user/create";
     }
 
 
     @PostMapping("/create") // this mapping helps to collect UI input userDTO and post it // use same endpoint as getMapping
-    public String insertUser(@ModelAttribute("userDTO") UserDTO userDTO){
+    public String insertUser(@Valid @ModelAttribute("userDTO") UserDTO userDTO, BindingResult bindingResult, Model model){
+
+        // validation
+        if (bindingResult.hasErrors()){
+            model.addAttribute("roles", roleService.listAllRoles());
+            model.addAttribute("users", userService.listAllUsers());
+            return "user/create";
+        }
 
         userService.save(userDTO); // save the user into DB
 
@@ -46,9 +54,9 @@ public class UserController {
         // get the username from the UI with pathVariable
         // define the attributes
 
-        model.addAttribute("userDTO", userService.findById(username));  // collect the data from UI into new UserDTO
-        model.addAttribute("roles", roleService.findAll()); // bring me all roles from DB
-        model.addAttribute("users", userService.findAll());
+        model.addAttribute("userDTO", userService.findByUsername(username));  // collect the data from UI into new UserDTO
+        model.addAttribute("roles", roleService.listAllRoles()); // bring me all roles from DB
+        model.addAttribute("users", userService.listAllUsers());
 
         return "/user/update";
     }
@@ -62,9 +70,9 @@ public class UserController {
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteUser(@PathVariable("id") String sameID){
+    public String deleteUser(@PathVariable("id") String username){
 
-        userService.deleteById(sameID);
+        userService.deleteByUsername(username);
 
         return "redirect:/user/create";
     }
