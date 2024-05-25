2e10d7f572add43dbec99b00670fb740ab460cbd
grigorerosca <gregory081093@gmail.com>
2024-05-25 09:19:57 -0400

    User Service

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,23 +1,37 @@
 package com.cydeo.service.impl;
 
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
 import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepo;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
+
 public class UserServiceImpl implements UserService {
 
     private final UserRepo userRepo;
     private final UserMapper userMapper;
+    private final ProjectService projectService;
+    private final TaskService taskService;
+
+    public UserServiceImpl(UserRepo userRepo, UserMapper userMapper, @Lazy ProjectService projectService, TaskService taskService) {
+        this.userRepo = userRepo;
+        this.userMapper = userMapper;
+        this.projectService = projectService;
+        this.taskService = taskService;
+    }
 
     @Override
     public List<UserDTO> listAllUsers() {
@@ -70,18 +84,43 @@ public class UserServiceImpl implements UserService {
     @Override
     public void delete(String username) {
 
+
         // we don't need to delete from DB any users or transactions
         // we need to change is-deleted = true
         //find user in DB
         User user = userRepo.findByUserName(username);
 
-        // set isDeleted=true
-        user.setIsDeleted(true);
+        if (checkIfUserCanBeDeleted(user)) {
 
-        // save updatedUser in DB
-        userRepo.save(user);
+            // set isDeleted=true
+            user.setIsDeleted(true);
+
+            user.setUserName(user.getUserName()+"-"+user.getId());
+            // save updatedUser in DB
+            userRepo.save(user);
+        }
 
     }
+
+
+    // this method is to check if the user has deleted status
+    private boolean checkIfUserCanBeDeleted(User user){
+        // we will use switch case
+        switch (user.getRole().getDescription()){
+
+            case "Manager":
+                List<ProjectDTO> projectDTO = projectService.readAllByAssignedManager(user);
+                return projectDTO.size() == 0;
+            case "Employee":
+                List<TaskDTO> taskDTO = taskService.readAllAssignedEmployee(user);
+                return taskDTO.size() == 0;
+            default:
+                return true;
+
+        }
+
+    }
+
 
     @Override
     public List<UserDTO> listAllUsersByRole(String role) {
