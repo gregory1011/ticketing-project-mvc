29292f4322354d79f897c43844be4c7dee421adf
grigorerosca <gregory081093@gmail.com>
2024-05-31 07:33:05 -0400

    User entity

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -2,9 +2,7 @@ package com.cydeo.entity;
 
 import com.cydeo.enums.Gender;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
